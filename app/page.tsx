a3700271781d2a9eba4bcead56ac38556ef4d06a
invisible-ghosts <backlink.player@gmail.com>
2025-04-08 09:55:58 +0500

    try again

--- app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from "next/navigation"
-
-export default function Home() {
-  // Redirect to the default language (English)
-  redirect("/en")
-}
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true, // or false if it's temporary
+      },
+    ];
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
